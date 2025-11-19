da79ff3d7b5d0f2c40146af443a704af5b05db99
@Nagmanicodes <patilnagmani1@gmail.com>
2025-11-19 19:02:46 +0530

    Add files via upload

---
+++ areaofcircle.c
@@ -0,0 +1,15 @@
+/* =============================
+Program 2: Area of Circle
+============================= */
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#include <stdio.h>
+#include <math.h>
+int main() {
+double r = 5.5;
+double area = M_PI * r * r;
+printf("Area = %.2f", area);
+return 0;
+}
+
