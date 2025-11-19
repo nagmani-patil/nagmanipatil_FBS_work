0019e28a4af098d3fb8e16f1b23408fd90ab5a06
@Nagmanicodes <patilnagmani1@gmail.com>
2025-11-19 19:01:40 +0530

    Add files via upload

---
+++ addtwonumber.c
@@ -0,0 +1,9 @@
+/* =============================
+Program 1: Add Two Integers
+============================= */
+#include <stdio.h>
+int main() {
+int a = 15, b = 27;
+printf("Sum = %d", a + b);
+return 0;
+}
